bdf0910bc94961a67f34b8049e458088cc84d9f7
Tatsuki711 <114969672+Tatsuki711@users.noreply.github.com>
2022-10-18 03:42:28 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
+  ellipse(50,50,50)
+  for(let i = 0; i < 5; i++){
+    stroke(255,0,0)
+    ellipse(50,50,100-i*10)
+    for(let j=5;j<10;j++){
+      stroke(0,0,255)
+      ellipse(50,50,100-j*10)
+          }
+    
     // BLANK[1]
   }
 }
