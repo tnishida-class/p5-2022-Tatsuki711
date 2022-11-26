d393bb27aa5ba200c05f9eb5b069dae78c1ae2e2
Tatsuki711 <114969672+Tatsuki711@users.noreply.github.com>
2022-11-26 15:01:22 +0900

    change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,17 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",2 , 10, 125, 125, 125);
 }
 
-function balloon(t){
+function balloon(t, x, y, cr, cg, cb){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  noStroke()
+  fill(cr, cg, cb);
+  rect(x, y, x + w + p, y + h + p);
+  triangle(x + w + p - 20, y + h + p + 10, x + w + p - 10, y+h+p+30, x+w+p-10, y+h+p+10 )
   fill(255);
   text(t, p, h + p);
 }
